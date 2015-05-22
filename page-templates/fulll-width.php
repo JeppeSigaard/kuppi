b29bcbcaec8fe5a824a1368b282a07c2cbe6d847
JeppeSigaard <jeppe@smatmonkey.dk>
2015-05-22 14:27:05 +0200

    opdateret id fra blog til page

--- page-templates/fulll-width.php
+++ page-templates/fulll-width.php
@@ -5,7 +5,7 @@ Template name: Fuld bredde
 get_header();
 ?>
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
-<section class="content full-width" role="directory" id="blog">
+<section class="content full-width" role="directory" id="page">
     <main class="main-full">
         <article <?php post_class(); ?>>
             <h1 class="post-title"><?php the_title();?></h1>
