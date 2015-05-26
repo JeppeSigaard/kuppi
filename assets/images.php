92632837e0b5634527224b6c83ee33a11ecbf5ef
JeppeSigaard <jeppe@smatmonkey.dk>
2015-05-26 17:00:58 +0200

    tilføjet preview narrow

--- assets/images.php
+++ assets/images.php
@@ -7,6 +7,7 @@ function smamo_image_setup() {
     add_image_size( 'preview-large', 640, 315, true );
     add_image_size( 'preview-medium', 320, 200, true );
     add_image_size( 'preview-small', 200, 125, true );
+    add_image_size( 'preview-narrow', 70, 120,true);
     add_image_size( 'kunstner-small', 180, 200, true );
 
 }
