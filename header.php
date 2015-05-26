c461c3e4ed9de3e614d82b0d313d9cbcb7cec390
JeppeSigaard <jeppe@smatmonkey.dk>
2015-05-26 16:23:36 +0200

    opdateret placering af splash

--- header.php
+++ header.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <script>kuppiLoadPage = setTimeout(function(){document.getElementsByTagName('body')[0].className+=" loading-screen";},400);</script>
     <?php wp_head();?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title(true,' · ', 'right'); ?></title>
 </head>
 <body <?php body_class(); ?>>
+<script>kuppiLoadPage = setTimeout(function(){document.getElementsByTagName('body')[0].className+=" loading-screen";},400);</script>
 <?php
 $nav_args = array(
 	'theme_location'  => 'header-menu',
