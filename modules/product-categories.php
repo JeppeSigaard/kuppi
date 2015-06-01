50e2888e2e5158eb77783b1f3734930c37028166
JeppeSigaard <jeppe@smatmonkey.dk>
2015-06-01 16:05:12 +0200

    produktkategorier

--- modules/product-categories.php
+++ modules/product-categories.php
@@ -1,5 +1,15 @@
 <?php
 
+$queried_object = get_queried_object();
+if((isset($queried_object->term_id))){
+    $current_category_id = $queried_object->term_id;
+}
+else{
+    $categories = get_the_terms(get_the_ID(),'product_cat');
+    $current_category_id = ($queried_object->post_type !== 'page') ? $categories[0]->term_id : 0;
+}
+
+
 $args = array(
     'taxonomy'     => 'product_cat',
     'orderby'      => 'name',
@@ -16,8 +26,8 @@ $all_categories = get_categories( $args );
     <div class="inner">
         <h3>Designs</h3>
         <ul>
-        <?php foreach ($all_categories as $cat) : $category_id = $cat->term_id; ?>
-            <li class="product-cat cat-<?php echo $cat->name ?>">
+            <?php foreach ($all_categories as $cat) : $category_id = $cat->term_id; $current = ($category_id == $current_category_id) ? ' current': '';?>
+            <li class="product-cat cat-<?php echo $cat->term_id . $current; ?>">
                 <a href="<?php echo get_term_link($cat->slug, 'product_cat') ?>"><?php echo  $cat->name; ?></a>
             </li>
         <?php endforeach; ?>
